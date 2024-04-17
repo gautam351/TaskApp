0ba17f0c607473e58c69ebef27479d39a2478159
Praveen Gautam <praveen.gautam.external@airbus.com>
2024-04-17 11:51:40 +0530

    Saftey commit

--- Backend/DalLayer/Models/TaskAppContext.cs
+++ Backend/DalLayer/Models/TaskAppContext.cs
@@ -25,7 +25,7 @@ namespace DalLayer.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=BLRKEC334053L\\SQLEXPRESS;Database=TaskApp;User Id=sa;Password=PraveenGautam@123");
+                optionsBuilder.UseSqlServer("Server=BLRKEC334053L\\SQLEXPRESS;Database=TaskApp;User Id=sa;Password=<Your_Password>");
             }
         }
 
